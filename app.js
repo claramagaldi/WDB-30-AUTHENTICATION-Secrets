1537eac572d7d213099692998c4b87f35637bc50
Clara <claramagaldi@gmail.com>
2022-02-08 16:16:17 -0500

    Level 4 - Hashing and Salting with bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express  = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const app = express();
 
 app.use(express.static("public"));
@@ -22,7 +24,6 @@ const userSchema = mongoose.Schema({
 
 const User = new mongoose.model("User", userSchema);
 
-
 app.get("/", function(req, res){
   res.render("home");
 });
@@ -33,18 +34,20 @@ app.route("/login")
   })
   .post(function(req, res){
       const username = req.body.username;
-      const password = md5(req.body.password);
+      const password = req.body.password;
       User.findOne({email: username}, function(err, foundUser){
         if(err){
           console.log(err);
         } else {
           if(foundUser){
-            if(foundUser.password === password) {
-              res.render("secrets");
-            };
-          };
+            bcrypt.compare(password, foundUser.password, function(err, result){
+              if(result === true) {
+                res.render("secrets");
+              }
+            });
+          }
         }
-      })
+      });
     });
 
 app.route("/register")
@@ -52,17 +55,19 @@ app.route("/register")
     res.render("register");
   })
   .post(function(req, res){
-    const newUser = new User({
-      email: req.body.username,
-      password: req.body.password
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
+      newUser.save(function(err){
+        if(err){
+          console.log(err);
+        } else {
+          res.render("secrets");
+        }
+      })
     });
-    newUser.save(function(err){
-      if(err){
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    })
   });
 
 app.listen(3000, function() {
