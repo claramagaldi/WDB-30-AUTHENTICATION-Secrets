ab49f5a1649f2a5eea6d5bce717af151ea3da0c1
Clara <claramagaldi@gmail.com>
2022-02-12 17:14:06 -0500

    Level 6 - Google OAuth 2.0 Authentication

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose"); //hash and salt passwords and save users into mongoDB Database
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
+// INITIAL CONFIGURATIONS
 const app = express();
 
 app.use(express.static("public"));
@@ -25,24 +29,106 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// CONNECTING TO LOCAL DATABASE
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
+// CREATING SCHEMA
+
 const userSchema = mongoose.Schema({
-  email: String,
-  password: String
+  username: { type: String, unique: true }, // values: email address, googleId, facebookId
+  password: String,
+  provider: String, // values: 'local', 'google', 'facebook'
+  email: String
 });
 
-userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose, { usernameField: "username"}); // LOCAL
+userSchema.plugin(findOrCreate); // GOOGLE & FACEBOOK
 
 const User = new mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+//STRATEGIES
+
+passport.use(User.createStrategy()); // LOCAL
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user){
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({ // GOOGLE
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate(
+      { username: profile.id },
+      {
+        provider: "google",
+        email: profile._json.email
+      },
+      function (err, user) {
+        return cb(err, user);
+      }
+    );
+  }
+));
+
+passport.use(new FacebookStrategy({ // FACEBOOK
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: "http://localhost:3000/auth/facebook/secrets",
+        profileFields: ["id", "email"]
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate(
+          { username: profile.id },
+          {
+            provider: "facebook",
+            email: profile._json.email
+          },
+          function (err, user) {
+            return cb(err, user);
+          }
+        );
+    }
+));
 
 app.get("/", function(req, res){
   res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get("/auth/facebook",
+    passport.authenticate("facebook", { scope: ["email"] })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect('/secrets');
+  }
+);
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  }
+);
 
 app.get("/secrets", function(req, res){
   if(req.isAuthenticated()){
@@ -76,7 +162,9 @@ app.route("/register")
     res.render("register");
   })
   .post(function(req, res){
-    User.register({username: req.body.username}, req.body.password, function(err, user){
+    const username = req.body.username;
+    const password = req.body.password;
+    User.register({username: username, email: username, provider: "local"}, password, function(err, user){
       if(err) {
         console.log(err);
         res.redirect("/register");
